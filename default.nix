1e264c0729424e37566eb382ae67ceb931297f57
xgroleau🐢 <xavgroleau@gmail.com>
2024-05-03 11:28:12 -0400

    feat: building roam (though bloated for now)

--- default.nix
+++ default.nix
@@ -1,5 +1,19 @@
-{ stdenv, fetchurl, dpkg, ... }:
+{ lib, stdenv, makeWrapper, fetchurl, dpkg, alsa-lib, atk, cairo, cups, dbus
+, expat, fontconfig, freetype, gdk-pixbuf, glib, pango, mesa, nspr, nss, gtk3
+, at-spi2-atk, gsettings-desktop-schemas, gobject-introspection, wrapGAppsHook
+, libsecret, libX11, libXScrnSaver, libXcomposite, libXcursor, libXdamage
+, libXext, libXfixes, libXi, libXrandr, libXrender, libXtst, libxcb
+, libxshmfence, nghttp2, libudev0-shim, glibc, curl, openssl, autoPatchelfHook
+}:
 let
+  runtimeLibs = lib.makeLibraryPath [
+    curl
+    glibc
+    libudev0-shim
+    nghttp2
+    openssl
+    stdenv.cc.cc.lib
+  ];
 in stdenv.mkDerivation rec {
   name = "ro.am";
   src = fetchurl {
@@ -8,13 +22,57 @@ in stdenv.mkDerivation rec {
     sha256 = "1faqb5vr1n6q9hlxpih5rgfxznygzsb8vz3bmsvjh1si6k4gzy13";
   };
 
-  buildInputs = [ dpkg ];
+  nativeBuildInputs =
+    [ autoPatchelfHook dpkg makeWrapper gobject-introspection wrapGAppsHook ];
 
-  unpackPhase = ''
-    dpkg-deb -x $src $out
-  '';
+  buildInputs = [
+    alsa-lib
+    at-spi2-atk
+    atk
+    cairo
+    cups
+    dbus
+    expat
+    fontconfig
+    freetype
+    gdk-pixbuf
+    glib
+    pango
+    gtk3
+    gsettings-desktop-schemas
+    libsecret.dev
+    libX11
+    libXScrnSaver
+    libXcomposite
+    libXcursor
+    libXdamage
+    libXext
+    libXfixes
+    libXi
+    libXrandr
+    libXrender
+    libXtst
+    libxcb
+    libxshmfence
+    mesa # for libgbm
+    nspr
+    nss
+  ];
+
+  dontBuild = true;
+  dontConfigure = true;
+  dontWrapGApps = true;
+  unpackPhase = "dpkg --fsys-tarfile $src | tar --extract";
 
   installPhase = ''
-    # Custom installation steps if necessary
+    mkdir -p $out
+    mv usr/* $out
+  '';
+
+  preFixup = ''
+    wrapProgramShell "$out/bin/roam" \
+        "''${gappsWrapperArgs[@]}" \
+        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland --enable-features=WaylandWindowDecorations}}" \
+        --prefix LD_LIBRARY_PATH : ${runtimeLibs}
   '';
 }
--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
-  description = "A very basic flake";
+  description = "Flake for ro.am";
 
   inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
 
   outputs = { self, nixpkgs }:
     let pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in { packages.x86_64-linux.roam = pkgs.callPackage ./default.nix { }; };
+    in { packages.x86_64-linux.default = pkgs.callPackage ./default.nix { }; };
 }
