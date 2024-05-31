89b7c0c2bbbfe60b01cefba8ebd9b7b2f7e417fc
xgroleau🐢 <xavgroleau@gmail.com>
2024-05-31 16:42:34 -0400

    feat: auto update roam

--- default.nix
+++ default.nix
@@ -1,8 +1,41 @@
-{ lib, stdenv, makeWrapper, fetchurl, dpkg, alsa-lib, dbus, fontconfig, freetype
-, glib, mesa, nspr, nss, gobject-introspection, wrapGAppsHook, libGL, libnotify
-, libsecret, libX11, libXScrnSaver, libXcomposite, libXcursor, libXdamage
-, libXext, libXfixes, libXi, libXrandr, libXrender, libXtst, libxcb
-, libxshmfence, nghttp2, libudev0-shim, glibc, curl, openssl, autoPatchelfHook
+{
+  lib,
+  stdenv,
+  makeWrapper,
+  fetchurl,
+  dpkg,
+  alsa-lib,
+  dbus,
+  fontconfig,
+  freetype,
+  glib,
+  mesa,
+  nspr,
+  nss,
+  gobject-introspection,
+  wrapGAppsHook,
+  libGL,
+  libnotify,
+  libsecret,
+  libX11,
+  libXScrnSaver,
+  libXcomposite,
+  libXcursor,
+  libXdamage,
+  libXext,
+  libXfixes,
+  libXi,
+  libXrandr,
+  libXrender,
+  libXtst,
+  libxcb,
+  libxshmfence,
+  nghttp2,
+  libudev0-shim,
+  glibc,
+  curl,
+  openssl,
+  autoPatchelfHook,
 }:
 let
   runtimeLibs = lib.makeLibraryPath [
@@ -14,18 +47,23 @@ let
     nghttp2
     openssl
     stdenv.cc.cc.lib
-
   ];
-in stdenv.mkDerivation rec {
+in
+stdenv.mkDerivation rec {
   name = "roam";
+  version = "0.22.0";
   src = fetchurl {
-    url =
-      "https://download.ro.am/Roam/8a86d88cfc9da3551063102e9a4e2a83/latest/linux/x64/Roam.deb";
+    url = "https://download.ro.am/Roam/8a86d88cfc9da3551063102e9a4e2a83/linux/debian/binary/111.1.2-beta001-roam_111.1.2-beta001_amd64.deb";
     sha256 = "sha256-ndxrZZI05jAv1EBSPuiMeGWswxK1g6yUebEbQfji1gU=";
   };
 
-  nativeBuildInputs =
-    [ autoPatchelfHook dpkg makeWrapper gobject-introspection wrapGAppsHook ];
+  nativeBuildInputs = [
+    autoPatchelfHook
+    dpkg
+    makeWrapper
+    gobject-introspection
+    wrapGAppsHook
+  ];
 
   buildInputs = [
     alsa-lib
--- flake.nix
+++ flake.nix
@@ -1,19 +1,24 @@
 {
   description = "Flake for ro.am";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
 
-  outputs = { self, nixpkgs }:
-    let pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in {
-      overlays.default = final: prev: {
-        roam = final.callPackage ./default.nix { };
-      };
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+
+      formatter.x86_64-linux = pkgs.nixfmt-rfc-style;
+
+      overlays.default = final: prev: { roam = final.callPackage ./default.nix { }; };
 
       packages.x86_64-linux = rec {
         roam = pkgs.callPackage ./default.nix { };
         default = roam;
       };
-
     };
 }
