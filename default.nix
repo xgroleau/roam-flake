ccf4682cd3cbed60b05ed19cd3b8f045b2d6b026
xgroleau <31734358+xgroleau@users.noreply.github.com>
2024-11-07 08:40:32 +0000

    Update ro.am hash

--- default.nix
+++ default.nix
@@ -51,10 +51,10 @@ let
 in
 stdenv.mkDerivation rec {
   name = "roam";
-  version = "134.0.0.beta001";
+  version = "134.5.0.beta001";
   src = fetchurl {
-    url = "https://download.ro.am/Roam/8a86d88cfc9da3551063102e9a4e2a83/linux/debian/binary/134.0.0-beta001-roam_134.0.0-beta001_amd64.deb";
-    sha256 = "0lxl7sm1x4d2fndwm9p1xy69is2xm7hp2p9v3d8p113r3w38li7r";
+    url = "https://download.ro.am/Roam/8a86d88cfc9da3551063102e9a4e2a83/linux/debian/binary/134.5.0-beta001-roam_134.5.0-beta001_amd64.deb";
+    sha256 = "1li0pbhp373g0crvplhlxx94l9xxwprwnvdlvjlva9lxbqbsl7m2";
   };
 
   nativeBuildInputs = [
